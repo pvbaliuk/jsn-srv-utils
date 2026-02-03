6f826af3d667bd4b807bfe4e0941c8d24f66c8fd
Pavlo Baliuk <pv.baliuk@gmail.com>
2026-02-03 18:00:16 +0200

    Refactor `useTsconfigPaths` to accept options object and improve path resolution logic

--- src/useTsconfigPaths.ts
+++ src/useTsconfigPaths.ts
@@ -1,10 +1,14 @@
 import {dirname, relative, resolve} from 'node:path';
 
+type UseTsconfigPathsOptions = {
+    basePath?: string;
+    tsconfigPath: string;
+}
+
 /**
- * @param {string} fromPath
- * @param {string} tsconfigPath
+ * @param {UseTsconfigPathsOptions} options
  */
-export function useTsconfigPaths(fromPath: string, tsconfigPath: string): void{
+export function useTsconfigPaths({basePath, tsconfigPath}: UseTsconfigPathsOptions): void{
     let moduleAlias: any|null = null;
     try{
         require('module-alias/register');
@@ -13,16 +17,23 @@ export function useTsconfigPaths(fromPath: string, tsconfigPath: string): void{
         throw new Error('module-alias package should be installed to use the function');
     }
 
-    const tsconfig = require(tsconfigPath),
+    const tsconfig = require(resolve(tsconfigPath)),
         tsconfigDirPathname = dirname(tsconfigPath),
         paths: Record<string, string> = {};
+
+    if(!basePath && !tsconfig?.compilerOptions?.baseUrl)
+        throw new TypeError('Either basePath option should be provided or tsconfig.json should contain compilerOptions.baseUrl property');
+
+    const baseUrl = tsconfig?.compilerOptions?.baseUrl
+        ? resolve(tsconfig.compilerOptions.baseUrl)
+        : resolve(basePath);
 
     Object.entries((tsconfig?.compilerOptions?.paths ?? {}) as Record<string, string[]>)
         .forEach(([moduleName, pathArray]) => {
             if(pathArray.length === 0)
                 return;
 
-            paths[moduleName] = resolve(tsconfigDirPathname, relative(tsconfigDirPathname, fromPath), pathArray[0]);
+            paths[moduleName] = resolve(tsconfigDirPathname, baseUrl, pathArray[0]);
         });
 
     moduleAlias.addAliases(paths);
