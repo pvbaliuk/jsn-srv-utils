01ac6a998819b01a2bfd9297c3db17dbbfeea7b6
Pavlo Baliuk <pv.baliuk@gmail.com>
2026-06-22 19:48:32 +0300

    Update connection schema to coerce port to a number; bump version to 1.3.1

--- src/config/predefined-schemas/connection.schema.ts
+++ src/config/predefined-schemas/connection.schema.ts
@@ -2,7 +2,7 @@ import {z} from 'zod';
 
 export const connectionSchema = z.object({
     host: z.string().nonempty(),
-    port: z.number().min(1).max(65535),
+    port: z.coerce.number().min(1).max(65535),
     user: z.string(),
     pass: z.string()
 });
